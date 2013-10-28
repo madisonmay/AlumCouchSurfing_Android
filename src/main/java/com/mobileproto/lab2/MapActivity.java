0ac0b3c235eafeb7370a88b35a08836ff00c4405
Madison May <madison.may@students.olin.edu>
2013-10-28 11:46:54 -0400

    Google Maps API

--- src/main/java/com/mobileproto/lab2/MapActivity.java
+++ src/main/java/com/mobileproto/lab2/MapActivity.java
@@ -3,7 +3,16 @@ package com.mobileproto.lab2;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 public class MapActivity extends Activity {
+    private GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
